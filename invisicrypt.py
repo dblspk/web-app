61490854b17422fb62564a4003d854f648be1ac8
Grond <grond66@riseup.net>
2016-11-12 11:58:36 -0800

    Refactor Nitzan's code to work in the main application

--- invisicrypt.py
+++ invisicrypt.py
@@ -5,13 +5,14 @@ import sys
 import argparse
 import random
 
+encoding_chars = [
+    "\u200B", # ZERO WIDTH SPACE
+    "\u200C", # ZERO WIDTH NON-JOINER
+    "\u200D", # ZERO WIDTH JOINER
+    "\uFEFF", # ZERO WIDTH NON-BREAKING SPACE
+]
+
 def encode_string(string):
-    encoding_chars = [
-        "\u200B", # ZERO WIDTH SPACE
-        "\u200C", # ZERO WIDTH NON-JOINER
-        "\u200D", # ZERO WIDTH JOINER
-        "\uFEFF", # ZERO WIDTH NON-BREAKING SPACE
-    ]
     output_string = io.StringIO()
     string_bytes = bytes(string, encoding="utf-8")
     for char in string_bytes:
@@ -19,15 +20,51 @@ def encode_string(string):
             output_string.write(encoding_chars[(char >> j) & 0x3])
     return output_string.getvalue()
 
-# complete dummies for now, use real code later
 def encode_text_message(text):
     return encode_string("STR:" + text)
+
+def decode_string(string):
+    back_to_tokens = [zero_char_to_token(ord(char)) for char in string]
+    return two_bit_list_to_string(back_to_tokens)
+
+def two_bit_list_to_string(lst):
+    byte_list = [int(lst[i] + lst[i+1] +
+        lst[i+2] + lst[i+3]) for i in range(0, len(lst)-2, 4)]
+    return byte_to_string(byte_list)
+
+def byte_to_string(byte_list):
+    byte_list = [int(str(elem), 2) for elem in byte_list]
+    return "".join(map(chr, byte_list))
+
+#Helper function
+def zero_char_to_token(zero_char_val):
+    return {
+        8203: '00',
+        8204: '01',
+        8205: '10',
+        65279:'11',
+    }[zero_char_val]
 
 def decode_text_message(text):
     return text
 
 def find_message_in_str(string):
-    return string
+    index_begin, index_end, index = 0, 0, 0
+    found_begin = False
+
+    #Finds beginning and end of ZW chars
+    for char in string:
+        if not found_begin and char in encoding_chars: #found first hidden char
+            index_begin = index
+            found_begin = True
+        elif found_begin and not char in encoding_chars: #found first non hidden char
+            index_end = index
+            break
+        index += 1
+    if found_begin and index_end == 0: #hidden chars reach end of message
+        index_end = index
+
+    return string[index_begin : index_end]
 
 root_parser = argparse.ArgumentParser(description="A utility for hiding and revealing messages in plain text")
 sub_parsers = root_parser.add_subparsers(dest="mode")
@@ -53,43 +90,6 @@ else: # options.mode == "reveal"
     encoded_message = find_message_in_str(options.combined_text)
     if not encoded_message:
         exit("It seems that there really isn't anything to see here...")
-    decoded_text = decrypt_message(encoded_message)
+    print("<" + encoded_message + ">")
+    decoded_text = decode_string(encoded_message)
     print(decoded_text)
-
-
-def decrypt_message(msg):
-    return decrypt(msg)
-
-
-def decrypt(received_message):
-    ZW = [8203, 8204, 8205, 65279]
-    index_begin, index_end, index = 0, 0, 0
-    found_begin = False
-
-
-    #Finds beginning and end of ZW chars
-    for char in received_message:
-        if not found_begin and ord(char) in ZW: #found first hidden char
-            index_begin = index
-            found_begin = True
-        elif found_begin and not ord(char) in ZW: #found first non hidden char
-            index_end = index
-            break
-        index += 1
-    if index_end == 0: #hidden chars reach end of message
-        index_end = index
-
-
-    message_to_decrypt = received_message[index_begin : index_end]
-    back_to_tokens = [zero_char_to_token(ord(char)) for char in message_to_decrypt]
-    return two_bit_list_to_string(back_to_tokens)
-
-
-    #Helper function
-def zero_char_to_token(zero_char_val):
-    return {
-        8203: '00',
-        8204: '01',
-        8205: '10',
-        65279:'11',
-    }[zero_char_val]
