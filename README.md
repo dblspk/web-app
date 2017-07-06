17be6c89f243dea5948cd43afeef1644a20a52a0
joshuaptfan <joshuaptfan@gmail.com>
2017-07-06 13:07:13 -0700

    Implement multi-message support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Embeds/extracts messages as zero width Unicode characters in text, as a form of [steganography](https://en.wikipedia.org/wiki/Steganography). UI is optimized for real time chat.
 
-__https://joshuaptfan.github.io/Doublespeak/__
+__https://joshuaptfan.github.io/doublespeak/__
 
 ## Usage
 
@@ -14,43 +14,35 @@ Output ciphertext is automatically copied by tabbing to or clicking on the field
 
 Fields automatically highlight on focus, type/paste to replace text. Clear buttons should not be needed, even on mobile.
 
-__Alt+A__ — focus input ciphertext
-
-__Alt+Z__ — focus and copy input hidden text
-
-__Alt+W__ — focus outgoing cover text
-
-__Alt+S__ — focus outgoing hidden text
-
-__Alt+X__ — focus and copy output ciphertext
-
 ## How it works
 
-[Unicode](https://en.wikipedia.org/wiki/Unicode) contains some zero width, unprintable characters. 4 of these are sufficiently resilient to input sanitation on the web to be useful:
 
-| Character | Description |
-| --------- | ----------- |
-| U+200B    | [zero width space](https://en.wikipedia.org/wiki/Zero-width_space) |
-| U+200C    | [zero width non-joiner](https://en.wikipedia.org/wiki/Zero-width_non-joiner) |
-| U+200D    | [zero width joiner](https://en.wikipedia.org/wiki/Zero-width_joiner) |
-| U+FEFF    | [zero width non-breaking space](https://en.wikipedia.org/wiki/Byte_order_mark) |
+[Unicode](https://en.wikipedia.org/wiki/Unicode) contains some zero width, unprintable characters. 4 of these are sufficiently resilient to input sanitation on the web to be useful. This allows us to encode any data in [quaternary](https://en.wikipedia.org/wiki/Quaternary_numeral_system). For text, the Unicode message to be hidden is converted to a bitstream, which is then encoded using our arbitrary encoding scheme:
 
-With 4 characters, we can encode any data in [quaternary](https://en.wikipedia.org/wiki/Quaternary_numeral_system). For text, the Unicode message to be hidden is converted to a bitstream, which is then encoded using our arbitrary encoding scheme:
+| Bits | Character | Description |
+| ---- | --------- | ----------- |
+| 00   | U+200B    | [zero width space](https://en.wikipedia.org/wiki/Zero-width_space) |
+| 01   | U+200C    | [zero width non-joiner](https://en.wikipedia.org/wiki/Zero-width_non-joiner) |
+| 10   | U+200D    | [zero width joiner](https://en.wikipedia.org/wiki/Zero-width_joiner) |
+| 11   | U+FEFF    | [zero width non-breaking space](https://en.wikipedia.org/wiki/Byte_order_mark) |
 
-| Bits | Character |
-| ---- | --------- |
-| 00   | U+200B    |
-| 01   | U+200C    |
-| 10   | U+200D    |
-| 11   | U+FEFF    |
-
-The resulting string of invisible characters is then inserted at a random location in the cover text.‌‌‌​​​​​‌​‍‌‌‍﻿‍‌‍‌​‌﻿‌‌‌‍​‍‌‍‍‌‌﻿‌​‌‍​‌‌‍​‍‌‍﻿​‌﻿‍‌​‍﻿‍
+The resulting string of invisible characters is then inserted at a random location in the cover text.
 
 ## Efficiency
 
 Each invisible character represents 2 bits, while taking 1 byte (8 bits) to store. Thus, the hidden data consumes 4 times as much memory as the original data, not including cover text.
 
+## Roadmap
+
+The following features are defined in the [protocol specification](https://docs.google.com/spreadsheets/d/1sx-kw7LFz4f7Qrtmo68lRi8_msIRVGsvGhiQuWppR4A/edit):
+
+* Simultaneous decoding of multiple chained messages
+* Encoding and decoding of files
+* Optional built-in encryption for convenience (message can be encrypted before encoding even without this feature)
+
 ## Credits
+
+This project began at [Cal Hacks 3.0](https://calhacks3.devpost.com/) by [a much less memorable name](https://devpost.com/software/invisicrypt).
 
 * [Joshua Fan](https://github.com/joshuaptfan) — web interface
 * [Samuel Arnold](https://github.com/Grond66) — encoding algorithm and Python interface
--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 var textarea = [];
 
+(function() {
+    var textareas = [
+        'out-hidden',
+        'out-cover',
+        'out-cipher',
+        'in-cipher',
+        'in-hidden'
+    ];
+    for (var i = 0; i < 5; i++)
+        textarea[i] = document.getElementById(textareas[i]);
+
+    resizeBody();
+    new Clipboard('.copy');
+    document.addEventListener('dragover', dragOverFile, false);
+    document.addEventListener('drop', dropFile, false);
+
+    if (/Mac|iP(hone|od|ad)/.test(navigator.userAgent)) {
+        textarea[2].placeholder = 'Copy [Command+C] output ciphertext';
+        textarea[3].placeholder = 'Paste [Command+V] input ciphertext';
+    }
+})()
+
+// Embed hidden message in cover text
 function embedData() {
-    var coverStr = textarea[1].value,
-        encodedStr = textarea[0].value != '' ? encodeText('D\u0000\u0000\u0000\u0000\u0000\u0001' + encodeLength(textarea[0].value.length) + textarea[0].value) : '',
-        insertPos = Math.floor(Math.random() * (coverStr.length - 1) + 1);
+    var coverStr = textarea[1].value;
+    var encodedStr = textarea[0].value != '' ? encodeText('D\u0000\u0000\u0000\u0000\u0000\u0001' + encodeLength(textarea[0].value.length) + textarea[0].value) : '';
+    var insertPos = Math.floor(Math.random() * (coverStr.length - 1) + 1);
     textarea[2].value = coverStr.slice(0, insertPos) + encodedStr + coverStr.slice(insertPos);
     resizeTextarea(textarea[2]);
+    // Flash textarea border
     textarea[2].classList.add('encode');
-    window.setTimeout(function() {
+    window.setTimeout(function () {
         textarea[2].classList.remove('encode');
     }, 200);
 }
 
-// encode length of data as variable length quantity in binary string form
-function encodeLength(n) {
-    var outputStr = String.fromCharCode(n & 0x7F);
-    while (n > 127) {
-        n >>= 7;
-        outputStr = String.fromCharCode(n & 0x7F | 0x80) + outputStr;
-    }
-    return outputStr;
-}
-
-function decodeLength(str) {
-    var length = 0;
-    for (var i = 0; i < str.length; i++)
-        length = length << 7 | str.codePointAt(i) & 0x7F;
-    return length;
-}
-
-function encodeText(str) {
-    var outputStr = '',
-        encodingChars = [
-            '\u200B', // zero width space
-            '\u200C', // zero width non-joiner
-            '\u200D', // zero width joiner
-            '\uFEFF'  // zero width non-breaking space
-        ];
-    for (var i = 0, sLen = str.length; i < sLen; i++)
-        for (var j = 6; j >= 0; j -= 2)
-            outputStr += encodingChars[(str.charCodeAt(i) >> j) & 0x3];
-    return outputStr;
-}
-
+// Extract received cipher data
 function initExtractData() {
     textarea[3].maxLength = 0x7FFFFFFF;
-    window.setTimeout(function() {
+    clearInHidden();
+    window.setTimeout(function () {
         // Discard cover text
         extractData(textarea[3].value.match(/[\u200B\u200C\u200D\uFEFF]/g));
     }, 1);
@@ -54,24 +49,27 @@ function initExtractData() {
 function extractData(array) {
     //console.log(decodeText(array));
     var t0 = performance.now();
-    // Discard cover text
     var encodingVals = {
-            '\u200B':0,
-            '\u200C':1,
-            '\u200D':2,
-            '\uFEFF':3
-        };
+        '\u200B': 0,
+        '\u200C': 1,
+        '\u200D': 2,
+        '\uFEFF': 3
+    };
+    // Check protocol signature and revision
     if (!array || decodeText(array.slice(0, 8)) != 'D\u0000') {
-        console.log('Protocol mismatch');
+        console.info('Protocol mismatch');
         return;
     }
-    var dataType = decodeText(array.slice(24, 28)),
-        VLQLen = 1;
+    // Read data type field
+    var dataType = decodeText(array.slice(24, 28));
+    // Get length of variable length quantity data length field
+    var VLQLen = 1;
     while (encodingVals[array[24 + VLQLen * 4]] > 1)
-        VLQLen ++;
+        VLQLen++;
     //console.log('VLQLen', VLQLen);
-    var dataLen = decodeLength(decodeText(array.slice(28, 28 + VLQLen * 4))),
-        dataEnd = 28 + (VLQLen + dataLen) * 4;
+    // Get length and end position of data field
+    var dataLen = decodeLength(decodeText(array.slice(28, 28 + VLQLen * 4)));
+    var dataEnd = 28 + (VLQLen + dataLen) * 4;
     //console.log('dataEnd', dataEnd, array.length);
 
     switch (dataType) {
@@ -81,22 +79,75 @@ function extractData(array) {
         case '\u0000':
         case '\u0002':
         default:
-            console.log('Only text extraction is supported at this time.')
+            console.info('Only text extraction is supported at this time.')
     }
-    console.log((performance.now() - t0).toFixed(2) + ' ms');
+    console.info('Decode: ' + (performance.now() - t0).toFixed(2) + ' ms');
 
+    // Recurse until all messages extracted    
     if (array.length > dataEnd)
         extractData(array.slice(dataEnd));
 }
 
+// 
+function outputText(array) {
+    var outputStr = decodeText(array);
+    if (textarea[4].lastChild.value) {
+        // Generate textarea
+        var ta = document.createElement('textarea');
+        ta.tabIndex = -1;
+        ta.readOnly = true;
+        textarea[4].appendChild(ta);
+    }
+    var ta = textarea[4].lastChild;
+    ta.value = outputStr;
+    resizeTextarea(ta);
+    // Flash textarea border
+    ta.classList.add('decode');
+    window.setTimeout(function () {
+        ta.classList.remove('decode');
+    }, 1000);
+}
+
+// encode length of data as variable length quantity in binary string form
+function encodeLength(n) {
+	var outputStr = String.fromCharCode(n & 0x7F);
+	while (n > 127) {
+		n >>= 7;
+		outputStr = String.fromCharCode(n & 0x7F | 0x80) + outputStr;
+	}
+	return outputStr;
+}
+
+// decode VLQ to integer
+function decodeLength(str) {
+	var length = 0;
+	for (var i = 0; i < str.length; i++)
+		length = length << 7 | str.codePointAt(i) & 0x7F;
+	return length;
+}
+
+function encodeText(str) {
+	var outputStr = '';
+	var encodingChars = [
+			'\u200B', // zero width space
+			'\u200C', // zero width non-joiner
+			'\u200D', // zero width joiner
+			'\uFEFF'  // zero width non-breaking space
+		];
+	for (var i = 0, sLen = str.length; i < sLen; i++)
+		for (var j = 6; j >= 0; j -= 2)
+			outputStr += encodingChars[(str.charCodeAt(i) >> j) & 0x3];
+	return outputStr;
+}
+
 function decodeText(array) {
-    var outputStr = '',
-        encodingVals = {
-            '\u200B':0,
-            '\u200C':1,
-            '\u200D':2,
-            '\uFEFF':3
-        };
+    var outputStr = '';
+    var encodingVals = {
+        '\u200B': 0,
+        '\u200C': 1,
+        '\u200D': 2,
+        '\uFEFF': 3
+    };
     for (var i = 0, sLen = array.length; i < sLen; i += 4) {
         var charCode = 0;
         for (var j = 0; j < 4; j++)
@@ -106,117 +157,75 @@ function decodeText(array) {
     return outputStr;
 }
 
-function outputText(array) {
-    var outputStr = decodeText(array);
-    textarea[4].value = outputStr;
-    resizeTextarea(textarea[4]);
-    textarea[4].classList.add('decode');
-    window.setTimeout(function() {
-        textarea[4].classList.remove('decode');
-    }, 1000);
-}
-
 function dragOverFile(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
+	e.stopPropagation();
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'copy';
 }
 
 function dropFile(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    
-    var file = e.dataTransfer.files[0],
-        reader = new FileReader();
-    reader.onload = function() {
-        console.log(new Uint8Array(reader.result));
-    };
-    reader.readAsArrayBuffer(file);
+	e.stopPropagation();
+	e.preventDefault();
+	
+	var file = e.dataTransfer.files[0];
+	var reader = new FileReader();
+	reader.onload = function() {
+		console.log(new Uint8Array(reader.result));
+	};
+	reader.readAsArrayBuffer(file);
 }
 
 function clearOutHidden() {
-    textarea[0].value = '';
-    resizeTextarea(textarea[0]);
-    embedData();
-    textarea[0].focus();
+	textarea[0].value = '';
+	resizeTextarea(textarea[0]);
+	embedData();
+	textarea[0].focus();
 }
 
 function clearOut() {
-    textarea[1].value = '';
-    resizeTextarea(textarea[1]);
-    embedData();
-    textarea[1].focus();
+	textarea[1].value = '';
+	resizeTextarea(textarea[1]);
+	embedData();
+	textarea[1].focus();
 }
 
 function clearIn() {
-    textarea[3].value = '';
-    textarea[4].value = '';
-    resizeTextarea(textarea[3]);
-    resizeTextarea(textarea[4]);
-    textarea[3].focus();
+    clearInHidden();
+	textarea[3].value = '';
+	resizeTextarea(textarea[3]);
+    resizeTextarea(textarea[4].lastChild);
+	textarea[3].focus();
+}
+
+function clearInHidden() {
+    textarea[4].firstChild.value = '';
+    while (textarea[4].childNodes.length > 1)
+        textarea[4].removeChild(textarea[4].lastChild);    
 }
 
 function notifyCopy(ta, copied) {
-    var copiedBanner = document.getElementById(copied);
-    textarea[ta].classList.add('copy');
-    copiedBanner.classList.add('show')
-    window.setTimeout(function() {
-        textarea[ta].classList.remove('copy');
-        copiedBanner.classList.remove('show');
-    }, 800)
+	var ta = document.getElementById(ta);
+	var copied = document.getElementById(copied);
+	ta.classList.add('copy');
+	copied.classList.add('show')
+	window.setTimeout(function() {
+		ta.classList.remove('copy');
+		copied.classList.remove('show');
+	}, 800)
 }
 
+// Scale elements according to viewport size
 function resizeBody() {
-    document.body.style.fontSize = Math.min(window.innerWidth, window.innerHeight) * 0.03 + 'px';
-    for (var i = 0; i < 5; i++)
+	document.documentElement.style.fontSize = Math.min(window.innerWidth, window.innerHeight) * 0.03 + 'px';
+	for (var i = 0; i < 4; i++)
         resizeTextarea(textarea[i]);
+    for (var i = 0, iLen = textarea[4].childNodes.length; i < iLen; i++)
+        resizeTextarea(textarea[4].childNodes[i]);    
 }
 
+// Scale textarea according to font size
 function resizeTextarea(el) {
-    var fontSize = parseFloat(document.body.style.fontSize);
-    el.style.height = '';
-    el.style.height = Math.min(el.scrollHeight + fontSize * 0.3, fontSize * 12) + 'px';
-}
-
-window.addEventListener('keyup', function(e) {
-    // Select textareas with keys
-    if (e.altKey)
-        switch (e.keyCode) {
-            case 81: // Alt+Q
-                textarea[0].focus();
-                break;
-            case 65: // Alt+A
-                textarea[1].focus();
-                break;
-            case 90: // Alt+Z
-                textarea[2].focus();
-                break;
-            case 83: // Alt+S
-                textarea[3].focus();
-                break;
-            case 88: // Alt+X
-                textarea[4].focus();
-                document.getElementById('in-copy').click();
-        }
-}, false);
-
-document.onreadystatechange = function() {
-    var textareas = ['out-hidden',
-                     'out-cover',
-                     'out-cipher',
-                     'in-cipher',
-                     'in-hidden'
-        ];
-    for (var i = 0; i < 5; i++)
-        textarea[i] = document.getElementById(textareas[i]);
-
-    resizeBody();
-    new Clipboard('.copy');
-    document.addEventListener('dragover', dragOverFile, false);
-    document.addEventListener('drop', dropFile, false);
-
-    if (navigator.userAgent.match(/Mac|iP(hone|od|ad)/)) {
-        textarea[2].placeholder = 'Copy [Command+C] output ciphertext';
-        textarea[3].placeholder = 'Paste [Command+V] input ciphertext';
-    }
+	var fontSize = parseFloat(document.documentElement.style.fontSize);
+	el.style.height = '';
+	el.style.height = Math.min(el.scrollHeight + fontSize * 0.3, fontSize * 12) + 'px';
 }
